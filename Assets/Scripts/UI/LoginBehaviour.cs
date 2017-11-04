e8d4687892637379bcb7f59faff6df4386ce4355
Gustavo F Oliveira <hi.gustavo.oliveira@Gustavos-MBP.hitronhub.home>
2017-11-04 02:07:45 -0700

    Add login feedback

--- Assets/Scripts/UI/LoginBehaviour.cs
+++ Assets/Scripts/UI/LoginBehaviour.cs
@@ -10,6 +10,8 @@ public class LoginBehaviour : UIBehaviour {
     [SerializeField]
     private GameObject button;
     [SerializeField]
+    private GameObject loadingAnimator;
+    [SerializeField]
     private GameObject errorMessage;
 
 	public override void GameStateChanged(GameState gameState)
@@ -17,15 +19,18 @@ public class LoginBehaviour : UIBehaviour {
         switch (gameState)
         {
             case GameState.Login:
+                loadingAnimator.SetActive(false);
                 usernameField.gameObject.SetActive(true);
                 passwordField.gameObject.SetActive(true);
                 button.SetActive(true);
                 errorMessage.SetActive(false);
                 break;
             case GameState.LoginError:
+                loadingAnimator.SetActive(false);
                 errorMessage.SetActive(true);
                 break;
             case GameState.Game:
+                loadingAnimator.SetActive(false);
 				usernameField.gameObject.SetActive(false);
 				passwordField.gameObject.SetActive(false);
                 button.SetActive(false);
@@ -36,6 +41,7 @@ public class LoginBehaviour : UIBehaviour {
 
     public void LoginButtonClicked ()
     {
+        loadingAnimator.SetActive(true);
         Login login = new Login(usernameField.text, passwordField.text);
         GameManagerBehaviour.instancie.TryLogIn(login);
     }
